5280013dd16b215bbd9d7f1cd912e945febcd1cd
Juan Calderon <juan.calderon@jalasoft.com>
2017-07-04 13:21:08 -0400

    Configure formatters, remove XML formatters. Test in http://localhost:40785/api/product for example

--- WebApplication/WebApplication/Startup.cs
+++ WebApplication/WebApplication/Startup.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.Owin;
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Newtonsoft.Json.Serialization;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication.Startup))]
+
 namespace WebApplication
 {
-    public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
             ConfigureAuth(app);
+
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+            formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            formatters.JsonFormatter.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
         }
     }
 }
