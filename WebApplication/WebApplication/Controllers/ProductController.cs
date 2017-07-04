ff9d6eee2e9095f91521a03b17d7cb0ca4e94d97
Juan Calderon <juan.calderon@jalasoft.com>
2017-07-04 13:01:04 -0400

    Add Product model and ProductController. Test in http://localhost:40785/api/product for example

---
+++ WebApplication/WebApplication/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private readonly Product[] products = {
+            new Product { Id = 1, Name = "Laptop", Description = "Mac", Quantity = 100 },
+            new Product { Id = 2, Name = "Smartphone", Description = "Samsung Galaxy s10", Quantity = 350 },
+            new Product { Id = 3, Name = "All in one", Description = "Microsoft Surface", Quantity = 60 }
+        };
+
+        // GET: api/product
+        public IHttpActionResult GetProducts()
+        {
+            return Ok(products);
+        }
+    }
+}
